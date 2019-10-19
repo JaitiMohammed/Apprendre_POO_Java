a5d42b089aca14e4e598d1d945192891b5ea8287
Jaiti Mohammed <38511742+JaitiMohammed@users.noreply.github.com>
2019-10-19 18:05:40 +0100

    Update commit

--- Exo1.java
+++ Exo1.java
@@ -1,12 +1,20 @@
-public class RacCarb { 
-  public static void main (String[] args){ 
-    double x ;
-  do{ System.out.print ("donnez un nombre positif : ") ;
-      x = Clavier.lireDouble() ;
-      if (x < 0) { System.out.println ("svp positif") ;
-      continue ;
-      }
-      if (x>0) System.out.println ("sa racine carree est : " + Math.sqrt (x) ) ;
-    }while (x != 0) ;
-  }
+package initiation;
+
+import java.util.Scanner;
+
+public class exo01 {
+	// Racine carree des nombres fournis en donnée .
+	
+	public static void main(String[] args) {
+	double x ;
+		Scanner Clavier = new Scanner(System.in);
+	  do{ System.out.print ("donnez un nombre positif : ") ;
+	      x = Clavier.nextDouble();
+	      if (x < 0) { System.out.println ("svp positif") ;
+	      continue ;
+	      }
+	      if (x>0) System.out.println ("sa racine carree est : " + Math.sqrt (x) ) ;
+	    }while (x != 0) ;
+	  }
 }
+
